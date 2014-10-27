1aa430adcfb309cf453da1f924235db2cc6b7178
Brittany Rompa <brittanyrompa@gmail.com>
2014-10-27 02:14:27 -0400

    line following

---
+++ lineFollower-VPS/lineFollower-VPS.ino
@@ -0,0 +1,89 @@
+#include <QTRSensors.h>
+#include <Wire.h>
+
+#include "utility/Adafruit_PWMServoDriver.h"
+
+
+
+/* 
+This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+It won't work with v1.x motor shields! Only for the v2's with built in PWM
+control
+
+For use with the Adafruit Motor Shield v2 
+---->	http://www.adafruit.com/products/1438
+*/
+
+
+#define NUM_SENSORS   8     // number of sensors used
+#define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
+#define EMITTER_PIN   2     // emitter is controlled by digital pin 2
+
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+// You can also make another motor on port M2
+//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+
+ PID myP(&Input, &Output, &Setpoint,2,5,1, DIRECT);
+ 
+ 
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  //motor1
+  myMotor1->setSpeed(150);
+  myMotor1->run(FORWARD);
+  
+  
+  //motor2
+  myMotor2->setSpeed(150);
+  myMotor2->run(FORWARD);
+  
+  QTRSensorsRC qtrrc((unsigned char[]) {3, 4, 5, 6, 7, 8, 9, 10},
+  NUM_SENSORS, TIMEOUT, EMITTER_PIN); 
+  
+  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
+  {
+    qtrrc.calibrate();       // reads all sensors 10 times at 2500 us per read (i.e. ~25 ms per call)
+  } 
+
+}
+
+void loop() {
+  
+    // read raw sensor values
+  qtrrc.read(sensorValues);
+
+  // print the sensor values as numbers from 0 to 2500, where 0 means maximum reflectance and
+  // 1023 means minimum reflectance
+  for (unsigned char i = 0; i < NUM_SENSORS; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t'); // tab to format the raw data into columns in the Serial monitor
+  }
+  Serial.println();
+  
+  delay(250);
+  
+  //Input = analogRead(0);
+  //myP.Compute();
+  //analogWrite(3,Output);
+}
+
+
+
